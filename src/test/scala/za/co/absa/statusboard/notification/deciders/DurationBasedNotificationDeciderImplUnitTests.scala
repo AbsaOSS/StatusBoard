efa8a63a0ec475513eb27c614e8671fa10afd4ba
Miroslav Chomut (CZ) <Miroslav.Chomut@absa.africa>
2025-05-27 08:41:40 +0200

    new rules for when to notify

--- src/test/scala/za/co/absa/statusboard/notification/deciders/DurationBasedNotificationDeciderImplUnitTests.scala
+++ src/test/scala/za/co/absa/statusboard/notification/deciders/DurationBasedNotificationDeciderImplUnitTests.scala
@@ -16,98 +16,104 @@
 
 package za.co.absa.statusboard.notification.deciders
 
-import za.co.absa.statusboard.model.NotificationCondition
+import org.mockito.Mockito.{mock, reset, when}
+import za.co.absa.statusboard.model.AppError.DatabaseError.RecordNotFoundDatabaseError
+import za.co.absa.statusboard.model.{NotificationCondition, RefinedStatus}
 import za.co.absa.statusboard.model.RawStatus.{Green, Red}
+import za.co.absa.statusboard.repository.StatusRepository
 import za.co.absa.statusboard.testUtils.{ConfigProviderSpec, TestData}
 import zio.test.Assertion.equalTo
-import zio.test.{Spec, TestEnvironment, assertZIO}
-import zio.Scope
+import zio.test.{Spec, TestAspect, TestEnvironment, assert}
+import zio.{Scope, Task, ZIO, ZLayer}
 
 import java.time.Duration
 
 object DurationBasedNotificationDeciderImplUnitTests extends ConfigProviderSpec {
-  private val statusNotSent10MinutesGreen = TestData.refinedStatus.copy(
-    status = Green("BAD"),
-    notificationSent = false,
-    lastSeen = TestData.refinedStatus.firstSeen.plus(Duration.ofMinutes(10))
+  private def statusGreen(sent: Boolean, seen: Int) = TestData.refinedStatus.copy(
+      status = Green("INFO"),
+      notificationSent = sent,
+      lastSeen = TestData.refinedStatus.firstSeen.plus(Duration.ofMinutes(seen))
+    )
+
+  private def statusRed(sent: Boolean, seen: Int) = TestData.refinedStatus.copy(
+    status = Red("INFO", intermittent = false),
+    notificationSent = sent,
+    lastSeen = TestData.refinedStatus.firstSeen.plus(Duration.ofMinutes(seen))
   )
 
-  private val statusNotSent10Minutes = TestData.refinedStatus.copy(
-    status = Red("BAD", intermittent = false),
-    notificationSent = false,
-    lastSeen = TestData.refinedStatus.firstSeen.plus(Duration.ofMinutes(10))
+  private def statusRedInt(sent: Boolean, seen: Int) = TestData.refinedStatus.copy(
+    status = Red("INFO", intermittent = true),
+    notificationSent = sent,
+    lastSeen = TestData.refinedStatus.firstSeen.plus(Duration.ofMinutes(seen))
   )
 
-  private val statusNotSentIntermittent10Minutes = TestData.refinedStatus.copy(
-    status = Red("BAD", intermittent = true),
-    notificationSent = false,
-    lastSeen = TestData.refinedStatus.firstSeen.plus(Duration.ofMinutes(10))
-  )
+  private def condition(seen: Int) = NotificationCondition.DurationBased(Duration.ofMinutes(seen).toSeconds.toInt)
 
-  private val statusYesSent10Minutes = TestData.refinedStatus.copy(
-    status = Red("BAD", intermittent = false),
-    notificationSent = true,
-    lastSeen = TestData.refinedStatus.firstSeen.plus(Duration.ofMinutes(10))
-  )
+  private val repositoryMock = mock(classOf[StatusRepository])
 
-  private val statusYesSentIntermittent10Minutes = TestData.refinedStatus.copy(
-    status = Red("BAD", intermittent = true),
-    notificationSent = true,
-    lastSeen = TestData.refinedStatus.firstSeen.plus(Duration.ofMinutes(10))
-  )
+  private def setupRepoMock(lastNotifiedStatus: RefinedStatus): Task[Unit] = ZIO.attempt {
+    reset(repositoryMock)
+    when(repositoryMock.getLatestNotifiedStatus(TestData.refinedStatus.env, TestData.refinedStatus.serviceName))
+      .thenReturn(ZIO.succeed(lastNotifiedStatus))
+  }
 
-  private val conditionDuration5Minutes = NotificationCondition.DurationBased(Duration.ofMinutes(5).toSeconds.toInt)
+  private def setupRepoMockEmpty(): Task[Unit] = ZIO.attempt {
+    reset(repositoryMock)
+    when(repositoryMock.getLatestNotifiedStatus(TestData.refinedStatus.env, TestData.refinedStatus.serviceName))
+      .thenReturn(ZIO.fail(RecordNotFoundDatabaseError("BAKA")))
+  }
 
-  private val conditionDuration30Minutes = NotificationCondition.DurationBased(Duration.ofMinutes(30).toSeconds.toInt)
+  private def resetRepoMock(): Task[Unit] = ZIO.attempt {
+    reset(repositoryMock)
+  }
 
   override def spec: Spec[TestEnvironment with Scope, Any] = {
     suite("DurationBasedNotificationDeciderImplSuite")(
-      test("Should not request notification, when already sent regardless of being over time") {
-        assertZIO(DurationBasedNotificationDecider.shouldNotify(conditionDuration5Minutes, statusYesSent10Minutes))(
-          equalTo(false)
-        )
+      test("Should yes request notification, when being stable (regardless of no overtime) and previously notified for different color") {
+        for {
+          _ <- setupRepoMock(statusGreen(sent = true, seen = 10))
+          response <- DurationBasedNotificationDecider.shouldNotify(condition(seen = 10), statusRed(sent = false, seen = 5))
+        } yield assert(response)(equalTo(true))
       },
-      test("Should not request notification, when already sent (intermittent) regardless of being over time") {
-        assertZIO(DurationBasedNotificationDecider.shouldNotify(conditionDuration5Minutes, statusYesSentIntermittent10Minutes))(
-          equalTo(false)
-        )
+      test("Should yes request notification, when being stable (regardless of no overtime) and previously no notification at all") {
+        for {
+          _ <- setupRepoMockEmpty()
+          response <- DurationBasedNotificationDecider.shouldNotify(condition(seen = 10), statusRed(sent = false, seen = 5))
+        } yield assert(response)(equalTo(true))
       },
-      test("Should not request notification, when already sent and not being over time") {
-        assertZIO(DurationBasedNotificationDecider.shouldNotify(conditionDuration30Minutes, statusYesSent10Minutes))(
-          equalTo(false)
-        )
+      test("Should yes request notification, when being intermittent, with overtime and previously notified for different color") {
+        for {
+          _ <- setupRepoMock(statusGreen(sent = true, seen = 10))
+          response <- DurationBasedNotificationDecider.shouldNotify(condition(seen = 5), statusRedInt(sent = false, seen = 10))
+        } yield assert(response)(equalTo(true))
       },
-      test("Should not request notification, when already sent (intermittnent) and not being over time") {
-        assertZIO(DurationBasedNotificationDecider.shouldNotify(conditionDuration30Minutes, statusYesSentIntermittent10Minutes))(
-          equalTo(false)
-        )
+      test("Should yes request notification, when being green (regardless of no overtime) and previously notified for different color (#160 negated in #5)") {
+        for {
+          _ <- setupRepoMock(statusRed(sent = true, seen = 10))
+          response <- DurationBasedNotificationDecider.shouldNotify(condition(seen = 10), statusGreen(sent = false, seen = 5))
+        } yield assert(response)(equalTo(true))
       },
-      test("Should not request notification, when being over time but green #160") {
-        assertZIO(DurationBasedNotificationDecider.shouldNotify(conditionDuration5Minutes, statusNotSent10MinutesGreen))(
-          equalTo(false)
-        )
+      test("Should not request notification, when already notified - also should not touch repo") {
+        for {
+          _ <- resetRepoMock()
+          response <- DurationBasedNotificationDecider.shouldNotify(condition(seen = 10), statusRed(sent = true, seen = 5))
+        } yield assert(response)(equalTo(false))
       },
-      test("Should yes request notification, when being over time") {
-        assertZIO(DurationBasedNotificationDecider.shouldNotify(conditionDuration5Minutes, statusNotSent10Minutes))(
-          equalTo(true)
-        )
+      test("Should not request notification, when being intermittent with no overtime - also should not touch repo") {
+        for {
+          _ <- resetRepoMock()
+          response <- DurationBasedNotificationDecider.shouldNotify(condition(seen = 10), statusRedInt(sent = false, seen = 5))
+        } yield assert(response)(equalTo(false))
       },
-      test("Should yes request notification, when being over time (intermittent)") {
-        assertZIO(DurationBasedNotificationDecider.shouldNotify(conditionDuration5Minutes, statusNotSentIntermittent10Minutes))(
-          equalTo(true)
-        )
+      test("Should not request notification, when previously notified for same color") {
+        for {
+          _ <- setupRepoMock(statusRed(sent = true, seen = 10))
+          response <- DurationBasedNotificationDecider.shouldNotify(condition(seen = 10), statusRed(sent = false, seen = 5))
+        } yield assert(response)(equalTo(false))
       },
-      test("Should yes request notification, when not being over time and NOT intermittent") {
-        assertZIO(DurationBasedNotificationDecider.shouldNotify(conditionDuration30Minutes, statusNotSent10Minutes))(
-          equalTo(true)
-        )
-      },
-      test("Should not request notification, when not being over time (intermittent)") {
-        assertZIO(DurationBasedNotificationDecider.shouldNotify(conditionDuration30Minutes, statusNotSentIntermittent10Minutes))(
-          equalTo(false)
-        )
-      }
-    )
-  }.provide(DurationBasedNotificationDeciderImpl.layer
+    ) @@ TestAspect.sequential
+  }.provide(
+    DurationBasedNotificationDeciderImpl.layer,
+    ZLayer.succeed(repositoryMock)
   )
 }
