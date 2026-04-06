dd2a0ee14b50143a87823e16f554adee2f6cae8d
Lukas Matta <lukasmatta0+git@gmail.com>
2026-04-06 20:30:38 +0200

    Fix tests

--- ui/jest.config.ts
+++ ui/jest.config.ts
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
   testEnvironment: 'jsdom',
   extensionsToTreatAsEsm: ['.ts'],
   testResultsProcessor: 'jest-teamcity-reporter',
